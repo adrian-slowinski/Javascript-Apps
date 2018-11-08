e2620388ff466b0505fe7f205c8cd41f81341d73
Adrian Słowiński <adrian.slowinski87@gmail.com>
2018-11-08 21:53:07 +0100

    Change variables names

--- Pesel/script.js
+++ Pesel/script.js
@@ -1,16 +1,16 @@
-let pesel = prompt("Please enter your ID number");
-let peselLength = pesel.length; 
+let idNumber = prompt("Please enter your ID number");
+let idNumberLength = idNumber.length; 
 
 function validatePesel(){ 
-  let total = pesel[0] * 1  + pesel[1] * 3 + pesel[2] * 7 + pesel[3] * 9 + pesel[4] * 1 + pesel[5] * 3 + pesel[6] * 7 + pesel[7] * 9 + pesel[8] * 1 + pesel[9] * 3;
+  let total = idNumber[0] * 1  + idNumber[1] * 3 + idNumber[2] * 7 + idNumber[3] * 9 + idNumber[4] * 1 + idNumber[5] * 3 + idNumber[6] * 7 + idNumber[7] * 9 + idNumber[8] * 1 + idNumber[9] * 3;
   total = total % 10;
   total = 10 - total;
   total = total % 10; 
 
-  if (pesel[pesel.length - 1] == total){
-    console.log(`Numer pesel (${pesel}) jest poprawny!`);
+  if (idNumber[idNumber.length - 1] == total){
+    console.log(`Your id number (${idNumber}) it's ok!`);
   } else {
-    console.log(`Numer pesel (${pesel}) jest niepoprawny!`);
+    console.log(`Your id number  (${idNumber}) it's incorrect!`);
   } 
 } 
 
