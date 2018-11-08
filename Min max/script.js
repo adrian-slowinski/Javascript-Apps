eaa800018af740593f760091e5d111cb1dcb3814
Adrian Słowiński <adrian.slowinski87@gmail.com>
2018-11-08 20:13:39 +0100

    Min/max two solutions

--- Min max/script.js
+++ Min max/script.js
@@ -2,21 +2,32 @@ const array = [1,14,15,-31,5,6,129,9,-21,55];
 let arrayLength = array.length;
 let min;
 let max;
-
-const showMaxMinNumber = () =>{
  
-    var max = Math.max(...array); 
-    var min = Math.min(...array); 
+const showMaxMinNumber = () =>{
+    max = Math.max(...array); 
+    min = Math.min(...array); 
     console.log(`Największa liczba to : ${max}`);
-    console.log(`Najmniejsza liczba to : ${min}`);
-
+    console.log(`Najmniejsza liczba to : ${min}`); 
 }
-
  
 showMaxMinNumber();
 
-
-
+function showMaxNumber2(){
+  max = array[0];
+  min = array[0];
+  for (let i = 0; i < arrayLength; i++){
+     if (array[i] < min){
+       min = array[i] ;
+     } else if (array[i] > max) {
+       max = array[i];
+     }
+  } 
+  console.log(`Największa liczba to : ${max}`);
+  console.log(`Najmniejsza liczba to : ${min}`); 
+ 
+}
+ 
+showMaxNumber2();
 
 
 
