c6226c3846e483e08bca40fd449ac325b77a9d88
Adrian Słowiński <adrian.slowinski87@gmail.com>
2018-11-11 22:28:22 +0100

    Analog Clock

---
+++ Analog Clock/script.js
@@ -0,0 +1,39 @@
+setInterval(function(){
+ 
+  let hour = document.querySelector(".clock__hour");
+  let minute = document.querySelector(".clock__minute");
+  let second = document.querySelector(".clock__second");
+  let currentDate = new Date();
+
+  let hours = currentDate.getHours();
+  let minutes = currentDate.getMinutes();
+  let seconds = currentDate.getSeconds();
+
+   function addZero(i) {
+     if (i < 10) {
+       i = "0" + i;
+     }
+     return i;
+   }
+
+  hour.innerHTML = addZero(hours);
+  minute.innerHTML = addZero(minutes);
+  second.innerHTML = addZero(seconds);
+}, 1000);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
