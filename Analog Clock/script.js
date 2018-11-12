04df70b9f6ec15fa290c5e3a034f8f1699ea7d91
Adrian Słowiński <adrian.slowinski87@gmail.com>
2018-11-12 14:52:32 +0100

    Analog Clock | red second card

--- Analog Clock/script.js
+++ Analog Clock/script.js
@@ -1,29 +1,31 @@
+ 
 setInterval(function(){
  
   let hour = document.querySelector(".clock__hour");
   let minute = document.querySelector(".clock__minute");
   let second = document.querySelector(".clock__second");
+  let second1 = document.querySelector(".clock__second--1");
+  let second2 = document.querySelector(".clock__second--2");
+  let second3 = document.querySelector(".clock__second--3");
+
   let currentDate = new Date();
 
   let hours = currentDate.getHours();
   let minutes = currentDate.getMinutes();
-  let seconds = currentDate.getSeconds();
-
+  let seconds = currentDate.getSeconds() ;  
+ 
    function addZero(i) {
      if (i < 10) {
        i = "0" + i;
      }
      return i;
    }
-
+ 
   hour.innerHTML = addZero(hours);
-  minute.innerHTML = addZero(minutes);
-  second.innerHTML = addZero(seconds);
+  minute.innerHTML = addZero(minutes); 
+  second.innerHTML = addZero(seconds); 
 }, 1000);
-
-
-
-
+ 
 
 
 
